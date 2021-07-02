4d326ebaaff6612d1d07d1558497e46e23e79de9
JoeWorsham <joeworsham@gmail.com>
2021-07-01 20:16:30 -0400

    Code sesh assignment one

---
+++ CodeSESH/CodeSESH/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CodeSESH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "hello world";
+            int count = greeting.Length;
+            
+            
+            // todo: goal one greeting and length printed out in console
+            // todo: goal two reverse order of greeting and print in console
+            // todo: goal three reverse greeting back and capitalize first letter in each word
+
+        }
+    }
+}
