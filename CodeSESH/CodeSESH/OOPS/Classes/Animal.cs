23e8bae99363245e0f60fad89cc93862fc437f97
Marcus Perez <marcus.a.perez@gmail.com>
2021-07-20 11:12:23 -0400

    Initial commit for intro to OOPS.

---
+++ CodeSESH/CodeSESH/OOPS/Classes/Animal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeSESH.OOPS.Classes
+{
+
+  public class Dog: AnimalBase
+  {
+
+  }
+
+  public class Cat: AnimalBase
+  {
+
+  }
+
+  public class AnimalBase
+  {
+    #region Properties
+
+    private int NumberOfMealsEaten { get; set; }
+    public string Name { get; set; }
+
+    #endregion
+
+
+    #region Functions
+    public bool Walk()
+    {
+      var result = false;
+
+      Console.WriteLine($"The animal named {Name} walked.");
+      result = true;
+
+      return result;
+    }
+
+    public bool Eat()
+    {
+      var result = false;
+
+      Console.WriteLine("Animal ate.");
+      
+      // short hand incrementing
+      NumberOfMealsEaten++;
+
+      // the long way
+      //NumberOfMealsEaten = NumberOfMealsEaten + 1;
+      result = true;
+
+      return result;
+    }
+    #endregion
+
+    #region Methods
+
+    public void PrintNumberOfMeals()
+    {
+      var myAnimalMealsEatenMessage = $"My animal named {Name} has eaten {NumberOfMealsEaten}";
+      Console.WriteLine(myAnimalMealsEatenMessage);
+    }
+
+    #endregion
+  }
+}
--- CodeSESH/CodeSESH/Program.cs
+++ CodeSESH/CodeSESH/Program.cs
@@ -1,31 +1,49 @@
 ﻿using System;
+using CodeSESH.OOPS.Classes;
 
 namespace CodeSESH
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
 
-            // todo: goal one greeting and length printed out in console
+      // todo: goal one greeting and length printed out in console
 
-            // var greeting = "Hello there stranger";
-            // Console.WriteLine(greeting);
-            // Console.WriteLine(greeting.Length);
+      // var greeting = "Hello there stranger";
+      // Console.WriteLine(greeting);
+      // Console.WriteLine(greeting.Length);
 
 
-            // todo: goal two reverse order of greeting and print in console
+      // todo: goal two reverse order of greeting and print in console
 
 
-            // Calling the method created in the StringReversal Class to output the results
-            StringReversal.StringReverseWithCharArray();
-            
-            
+      // Calling the method created in the StringReversal Class to output the results
+      //StringReversal.StringReverseWithCharArray();
 
 
+      // todo: goal three reverse greeting back and capitalize first letter in each word
 
-            // todo: goal three reverse greeting back and capitalize first letter in each word
 
-        }
+      //OOPS
+
+      var myCat = new Cat();
+      myCat.Name = "Althea";
+      
+      myCat.Walk();
+
+
+      var myDog = new Dog();
+      myDog.Name = "Louis";
+
+      myDog.Eat();
+
+      //var myDogMealsEatenMessage = $"My dog named {myDog.Name} has eaten {myDog}";
+      //Console.WriteLine(myDogMealsEatenMessage);
+
+      myDog.PrintNumberOfMeals();
+
+
     }
+  }
 }
